6d0b140f8048ad42c07cbf5d95fa0718518cd7fb
John Baer <johnbaer3@gmail.com>
2020-09-02 18:36:05 -0700

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by John Baer on 9/2/20.
+//  Copyright © 2020 John Baer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
